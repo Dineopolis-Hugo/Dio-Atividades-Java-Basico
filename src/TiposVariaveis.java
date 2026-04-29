0aa8f5c24852cc61d69598e79779ac2dd459e61b
Dineopolis Hugo <hugoamorimfilho@gmail.com>
2026-04-29 12:40:46 -0300

    Atualizaçao tipos de variaveis

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,27 @@
 public class TiposVariaveis {
     public static void main(String[] args){
-        System.out.println("funcionando");
+        double salarioMinimo = 2500.30;
+       short numeroCurto = 1;
+       int numeroNormal = numeroCurto;
+       short numeroCurto2 =  (short) numeroNormal;
+
+       int numero = 1;
+         numero = 10;
+        System.out.println(numero);
+
+        // double VALOR_DE_PI = 3.14;
+           // VALOR_DE_PI = 10.75;
+            // Aqui o valor da variavel pode mudar \\
+
+        final double PI = 3.14;
+            /*Com o "FINAL" a variavel se torna uma CONSTANTE
+             *(NOME DA CONSTANTE SÓ PODE SER ESCRITO EM CAIXA ALTA)
+             * e seu valor não pode ser mudado
+             */
+
+
+
+
+         //Valor nunca poderá mudar
     }
 }
