8f9466df3ad294258915d7ebc5e7c6e7be045fd5
Dineopolis Hugo <hugoamorimfilho@gmail.com>
2026-05-05 14:38:52 -0300

    ADD package, java docs e atividades

---
+++ ATIVIDADES/src/JAVADoc/Calculadora.java
@@ -0,0 +1,27 @@
+package JAVADoc;
+    /**
+     *<h1>calculadora</h1>
+     * A calculadora realiza operações mátemáticas emtre números inteiros
+     * <p>
+     * <b>Note:</b> Leia atentamente a documentação destas classes
+     * para desfrutar dos recursos oferecidos pelo autor
+     *
+     * @author Gleyson Sampaio
+     * @version 1.0
+     * @since 01/01/2022
+     */
+public class Calculadora {
+        /**
+         * este método é utilizado para somar dois números inteiros
+         *
+         * @param numeroUm   este é o primeiro parâmeterodo método
+         * @param numeroDois este é o segundo parâmetro do método
+         * @return int o resultado deste método é a soma dois dois números
+         */
+        public int somar(int numeroUm, int numeroDois) {
+            return numeroUm + numeroDois;
+        }
+
+
+    }
+
---
+++ ATIVIDADES/src/JAVADoc/comentarios.java
@@ -0,0 +1,26 @@
+package JAVADoc;
+
+public class comentarios {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário de uma única linha
+
+        /* Olá
+         * Eu sou um comentário
+         * que posso ser mais detalhado
+         * quando necessário
+         */
+
+        /**
+         * Essas duas estrelinhas acima
+         * É para identificar que você
+         * pretende elaborar um comentário
+         * A nível de documentação
+         * Que incrível !!
+         */
+       // Documentação geralmente fica em cima de métodos
+
+        }
+
+        }
+
+
