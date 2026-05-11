ac6d8dc9397b7bc8960a8640df5bb27501cc1c46
Dineopolis Hugo <hugoamorimfilho@gmail.com>
2026-05-11 17:05:55 -0300

    ATUALIZAÇÃO AboutMe (ADD Scanner)

--- ATIVIDADES/src/Terminal_E_Argumentos/AboutMe.java
+++ ATIVIDADES/src/Terminal_E_Argumentos/AboutMe.java
@@ -1,12 +1,25 @@
 package Terminal_E_Argumentos;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) {
 
-        String nome = args [0];
-        String sobrenome = args [1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("DIgite seu nome: ");
+            String nome = scanner.next();
+
+        System.out.print("Digite seu sobrenome: ");
+            String sobrenome = scanner.next();
+
+        System.out.print("Diigite sua altura: ");
+            double altura = scanner.nextDouble();
+
+        System.out.print("Digite sua idade: ");
+            int idade = scanner.nextInt();
+
 
         System.out.println("Olá, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
